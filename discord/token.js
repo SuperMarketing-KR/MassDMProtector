94c218571c2dccbd74d425e0e1bd863c0d6ce30a
SuperMarketing-KR <84508999+SuperMarketing-KR@users.noreply.github.com>
2021-06-07 17:47:08 +0900

    Update token.js

--- discord/token.js
+++ discord/token.js
@@ -3,13 +3,16 @@ const config = require("../config/config");
 
 module.exports = {
   readTokens: () => {
-    return fs
-      .readFileSync(config.TOKEN_PATH, "utf-8")
-      .trim()
-      .split("\n")
-      .map((e) => {
-        if (e.split(":").length == 3) return e.split(":")[2];
-        return e;
-      });
+    return (
+      fs
+        .readFileSync(config.TOKEN_PATH, "utf-8")
+        .trim()
+        // .split("\n")
+        .split(/\r?\n/)
+        .map((e) => {
+          if (e.split(":").length == 3) return e.split(":")[2];
+          return e;
+        })
+    );
   },
 };
